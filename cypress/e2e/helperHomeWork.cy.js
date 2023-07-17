f51f371b1bbe103b46d3b671b36c20492c84ca12
Yuliia Petrenko <juliapetrenko66@gmail.com>
2023-07-17 13:46:31 +0100

    Commit

--- cypress/e2e/helperHomeWork.cy.js
+++ cypress/e2e/helperHomeWork.cy.js
@@ -1,8 +1,22 @@
 import user from '../fixtures/user.json';
 import productList from '../fixtures/product.json';
-import { faker } from '@faker-js/faker';
+//import { faker } from '@faker-js/faker';
 import { loginViaUI } from '../support/helper.js';
 import { findProductByName, proceedToCheckout } from '../support/searchProductHelper.js';
+
+// user.address = faker.location.streetAddress();
+//   user.city = faker.location.city();
+//   user.company = faker.company.name();
+//   user.email = faker.internet.email();
+//   user.fax = faker.phone.number();
+//   user.firstName = faker.person.firstName();
+//   user.lastName = faker.person.lastName();
+//   user.loginName = faker.internet.userName();
+//   user.password = faker.internet.password({length: 20});
+//   user.phoneNumber = faker.phone.number('+380## ### ## ##');
+//   user.zipCode = faker.location.zipCode();
+
+//   let username = faker.internet.userName();
 
 
 it('Place order', () => {
